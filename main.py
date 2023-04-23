76e0ecf4823bb7f1c8d72f05b22ec4734ab4bf91
mafefreeitas <128387890+mafefreeitas@users.noreply.github.com>
2023-04-22 23:58:46 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+i= int(input("digite sua idade:"))
+eita=True
+if i<0:
+    eita=False
+    print("impossível")
+if i< 18:
+     eita=False
+     print("não precisa se alistar")
+if i>18 and i<65:
+    eita=False
+    print("nao esqueça e votar na proxima eleiçao.")
+if i>65:
+    eita=False
+    print("vá descansar")
+if eita:
+    print("eita!")
